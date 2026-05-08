745c4c9d4f11cf72918a88f7c53b03cdec38f5c4
Hipotea Dev <dev@hipotea.local>
2026-05-08 14:43:52 +0200

    kommoWebhookAction

--- Controller/KommoController.php
+++ Controller/KommoController.php
@@ -134,7 +134,7 @@ class KommoController extends Controller
      * 2. contacts.add → Contact creado (id en contacts.add[0].id)
      * 3. leads.add → Lead creado (linked_leads_id apunta al contact)
      */
-    public function kommoWebhookAction(Request $request)
+    public function kommoWebhookAction1(Request $request)
     {
         error_log('ENtro11111111111');
         $content = null;
@@ -346,6 +346,14 @@ class KommoController extends Controller
                 'timestamp' => date('Y-m-d H:i:s')
             ], 400);
         }
+    }
+
+    public function kommoWebhookAction(Request $request)
+    {
+        return new JsonResponse([
+            'ok' => true,
+            'error' => 'Método no implementado',
+        ], 200);
     }
 
     /**
